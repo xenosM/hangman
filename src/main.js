9198ec3f9eff783ab1bfb374b7b1be6d62124d2b
xenosM <105599352+xenosM@users.noreply.github.com>
2025-01-09 19:16:40 +0545

    HTML and CSS finished

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+//!VARIABLE DECLARATION
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//!FUNCTION DECLARATION
+async function getWord(){
+  const response =await fetch("https://random-word-api.vercel.app/api?words=1");
+  const word = await response.json()
+  return word
+}
 
-setupCounter(document.querySelector('#counter'))
+//!MAIN CODE
+async function main(){
+  const [word]=await getWord(); 
+}
+main()
