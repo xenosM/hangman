8014733f2a87af25f28302ec49d9401d8b844892
xenosM <105599352+xenosM@users.noreply.github.com>
2025-01-10 15:34:15 +0545

    Project Finished

--- src/main.js
+++ src/main.js
@@ -3,34 +3,41 @@ const rightAnswerContainer = document.querySelector("#right-answer-container");
 const wrongAnswerContainer = document.querySelector("#wrong-answer-container");
 const wrongAnswerResult = document.querySelector("#wrong-answer");
 const popup = document.querySelector("#popup");
-const bodyParts = document.querySelectorAll(".bodypart")
-console.log(bodyParts)
-
-let usedLetters = [];
-let mistakeCount =0;
+const bodyParts = document.querySelectorAll(".bodypart");
+const modalContainer = document.querySelector("#modal-container");
+const gameoverModal = document.querySelector("#gameover-modal");
+const winModal = document.querySelector("#win-modal");
+const restartBtn = document.querySelectorAll(".restart-btn");
+const answerDisplay = document.querySelector("#answer-display");
+let playState = true;
+let rightAnswers = [];
 let wrongAnswers = [];
+let mistakeCount = 0;
 //!FUNCTION DECLARATION
 async function main() {
   const [word] = await getWord();
-  console.log(word);
   generateBlankDash(word);
+  const blankSpace = document.querySelectorAll(".blank-space");
+
   //*Event Listener
   document.body.addEventListener("keydown", (e) => {
-    if (e.keyCode >= 65 && e.keyCode <= 90) {
+    if (/[a-z]/i.test(e.key) && playState) {
       let regex = new RegExp(`[${word}]`);
       let key = e.key.toLowerCase();
 
-      if (usedLetters.includes(key)) {
+      if (wrongAnswers.includes(key) || rightAnswers.includes(key)) {
         showPopup();
+        return;
       } else if (regex.test(key)) {
         rightAnswerDisplay(key, document.querySelectorAll(".blank-space"));
+        if ([...blankSpace].map((item) => item.innerText).join("") == word) {
+          gameWin();
+        }
       } else {
-        wrongAnswerContainer.style.visibility = "visible";
         wrongAnswerDisplay(key);
-        displayBodyPart();
-      }
-      if(mistakeCount>=6){
-       gameOver()
+        if (mistakeCount >= 6) {
+          gameOver(word);
+        }
       }
     }
   });
@@ -55,26 +62,40 @@ function rightAnswerDisplay(key, elements) {
       element.innerText = key;
     }
   });
-  usedLetters.push(key);
+  rightAnswers.push(key);
 }
 function wrongAnswerDisplay(key) {
-  usedLetters.push(key);
-  wrongAnswers.push(key);
+  wrongAnswerContainer.style.visibility = "visible";
   wrongAnswerResult.innerText = wrongAnswers;
+  wrongAnswers.push(key);
+  displayBodyPart();
 }
-function displayBodyPart(){
-  bodyParts[mistakeCount].style.display="block"
-  mistakeCount++
+function displayBodyPart() {
+  bodyParts[mistakeCount].style.display = "block";
+  mistakeCount++;
 }
 function showPopup() {
-  console.log("pop")
   popup.classList.add("show");
   setTimeout(() => {
     popup.classList.remove("show");
   }, 1200);
 }
-function gameOver(){
-  
+function gameOver(word) {
+  modalContainer.style.visibility = "visible";
+  gameoverModal.style.display = "block";
+  answerDisplay.innerText = `The answer was ${word}`;
+  playState = false;
+  restartBtn.forEach((item) => (item.onclick = restartGame));
 }
+function gameWin() {
+  modalContainer.style.visibility = "visible";
+  winModal.style.display = "block";
+  playState = false;
+  restartBtn.forEach((item) => (item.onclick = restartGame));
+}
+function restartGame() {
+  location.reload();
+}
+
 //!MAIN CODE
 main();
